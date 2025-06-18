4c6a1ac74e8ae01c3e775bb5607c80aa0055fd24
ArtemBuyalo <artem.buyalo@mail.mcgill.ca>
2025-06-18 16:36:34 -0400

    Pre-emptive fix for using wmo for missing values

--- workflow_obs.py
+++ workflow_obs.py
@@ -13,6 +13,19 @@ import xclim
 import xscen as xs
 from xscen.config import CONFIG
 
+def clean_for_zarr(ds: xr.Dataset) -> xr.Dataset:
+    """Clean dataset for Zarr saving: fix encodings and rechunk any multi-chunk variable."""
+    for var in ds.variables:
+        ds[var].encoding.pop("chunks", None)
+
+        da = ds[var]
+        if hasattr(da, "chunks") and da.chunks is not None:
+            # If any dimension has multiple chunks, rechunk fully
+            if any(len(dim_chunks) > 1 for dim_chunks in da.chunks):
+                ds[var] = da.chunk({dim: -1 for dim in da.dims})
+
+    return ds
+
 # Load configuration
 xs.load_config(
     "paths_obs.yml", "config_obs.yml", verbose=(__name__ == "__main__"), reset=True
@@ -108,6 +121,9 @@ if __name__ == "__main__":
                 
                 # create module with only indicators that are available for this input
                 cur_mod = xs.indicators.select_inds_for_avail_vars(ds_input, CONFIG["indicators"]["path_yml"])
+
+                ds_input = ds_input.unify_chunks()
+
                 # compute indicators and write to disk individually, this way it is easier to add more later
                 for name, ind in cur_mod.iter_indicators():
                     outfreq = ind.injected_parameters["freq"]
@@ -126,6 +142,8 @@ if __name__ == "__main__":
                             ds=ds_input,
                             indicators=[(name, ind)],
                         ).popitem()
+                        
+                        ds_ind = clean_for_zarr(ds_ind)
 
                         xs.save_and_update(ds=ds_ind, pcat=pcat,
                             path=CONFIG['paths']['task'],save_kwargs=CONFIG["indicators"]["save"])
